dae6e0684ddd5c14ff0edfa1b430a56fb7371d23
Michael Su <xmichael.su@gmail.com>
2020-03-30 20:58:30 -0700

    implement solutions for section 8, problem set WIP

---
+++ section-8-recursion-problem-set.js
@@ -0,0 +1,29 @@
+// ######### Power #########
+
+function power(base, expo) {
+  if (expo === 0) return 1;
+  return base * power(base, expo - 1);
+}
+
+// ######### Factorial #########
+
+function factorial(num) {
+  if (num < 1) return 1;
+  return num * factorial(num - 1);
+}
+
+// ######### productOfArray #########
+
+function productOfArray(array) {
+  if (array.length === 0) return 1;
+  return array[0] * productOfArray(array.slice(1));
+}
+
+// ######### Recursive Range #########
+
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
+
+// ######### Fib #########
